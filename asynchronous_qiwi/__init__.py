5847a8d4008493656e973e5283888a4e57234962
Kratos <LexLuthorReal@localhost.localdomain>
2021-05-22 15:04:26 -0700

    Version Limitation

--- asynchronous_qiwi/__init__.py
+++ asynchronous_qiwi/__init__.py
@@ -1,1 +1,12 @@
+import sys
+if sys.version_info < (3, 7):
+    raise ImportError('Your Python version {0} is not supported by aiogram, please install '
+                      'Python 3.7+'.format('.'.join(map(str, sys.version_info[:3]))))
+
+
 from .call import Wallet, P2P, Terminals
+
+
+__all__ = ('Wallet', 'P2P', 'Terminals')
+
+__version__ = '1.0.2'
